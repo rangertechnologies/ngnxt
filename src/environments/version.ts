5c58b61a9854acf42d0eb329c6bc039b073c7462
revathy2 <revathy@rangerinc.cloud>
2020-09-10 18:36:19 +0530

    Revert "second level commit"
    This reverts commit 56c45924357b729236c3aa2cddda710d7ff3f864.

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,13 +2,13 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": false,
-    "raw": "da45c36",
-    "hash": "da45c36",
+    "raw": "03703f9",
+    "hash": "03703f9",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "da45c36",
+    "suffix": "03703f9",
     "semverString": null,
-    "version": "1.0.4"
+    "version": "1.0.3"
 };
 /* tslint:enable */
