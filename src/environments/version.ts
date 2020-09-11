d11e026df330e434a0077c73de83016f2d79c257
lalkumar <lalkumar1020@gmail.com>
2020-09-11 17:28:52 +0530

    COS-84 issue fix

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,14 +1,14 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "03703f9",
-    "hash": "03703f9",
+    "dirty": true,
+    "raw": "b38f6e9-dirty",
+    "hash": "b38f6e9",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "03703f9",
+    "suffix": "b38f6e9-dirty",
     "semverString": null,
-    "version": "1.0.3"
+    "version": "1.0.5"
 };
 /* tslint:enable */
