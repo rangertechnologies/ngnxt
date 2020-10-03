037169048a5e28173b0f840a97c8e9bb192d8589
Riyaz <riyaz.mohamed@outlook.com>
2020-10-03 16:11:30 +0530

    added prio org

--- gulpfile.js
+++ gulpfile.js
@@ -59,14 +59,14 @@ gulp.task('create-package', function () {
     .pipe(gulp.dest('package/'));
 });
 
-gulp.task('dev-staticresources', function () {
+gulp.task('sandbox-staticresources', function () {
   return gulp.src('./'+distPath+'/**')
     .pipe(zip(`${dev_resources}.resource`))
     .pipe(file(`${dev_resources}.resource-meta.xml`, resourcesMetaXML))
     .pipe(gulp.dest('package/staticresources/'));
 });
 
-gulp.task('pkg-staticresources', function () {
+gulp.task('production-staticresources', function () {
   return gulp.src('./'+distPath+'/**')
     .pipe(zip(`${pkg_resources}.resource`))
     .pipe(file(`${pkg_resources}.resource-meta.xml`, resourcesMetaXML))
@@ -93,5 +93,15 @@ gulp.task('pkg-deploy', function () {
     }))
 });
 
-gulp.task('build-dev-static', gulp.series('create-package', 'dev-staticresources'))
-gulp.task('build-pkg-static', gulp.series('create-package', 'pkg-staticresources'))
+gulp.task('prio-deploy', function () {
+  return gulp.src('./package/**', { base: "." })
+    .pipe(zip('package.zip'))
+    .pipe(forceDeploy({
+      username: process.env.PRIO_USERNAME,
+      password: process.env.PRIO_PASSWORD,
+      loginUrl: process.env.PRIO_URL
+    }))
+});
+
+gulp.task('build-dev-static', gulp.series('create-package', 'sandbox-staticresources'))
+gulp.task('build-prod-static', gulp.series('create-package', 'production-staticresources'))
