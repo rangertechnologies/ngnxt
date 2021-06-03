d3b777d18df7e1242f72623a85b28b6dc536f7c6
sathiq <sathiq@rangerinc.co>
2021-06-03 12:18:48 +0530

    version update

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,13 +2,13 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "e288428-dirty",
-    "hash": "e288428",
+    "raw": "7a6cffb-dirty",
+    "hash": "7a6cffb",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "e288428-dirty",
+    "suffix": "7a6cffb-dirty",
     "semverString": null,
-    "version": "2.0.12"
+    "version": "2.0.13"
 };
 /* tslint:enable */
