497fd3f69023dbdcfe4741cd92212047a9642e41
lalkumar <lalkumar@rangerinc.co>
2022-08-23 14:14:43 +0530

    ng update @angular/cdk@11

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -2,12 +2,12 @@
 /* tslint:disable */
 export const VERSION = {
     "dirty": true,
-    "raw": "a25db01-dirty",
-    "hash": "a25db01",
+    "raw": "b8e2ffa-dirty",
+    "hash": "b8e2ffa",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "a25db01-dirty",
+    "suffix": "b8e2ffa-dirty",
     "semverString": null,
     "version": "2.0.11"
 };
