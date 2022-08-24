1f4c4fa101a7e6bb9c9ef3c8d5b44467890469ea
lalkumar <lalkumar@rangerinc.co>
2022-08-24 11:28:42 +0530

    NG version update

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "bad02ca",
-    "hash": "bad02ca",
+    "dirty": true,
+    "raw": "39a5a6b-dirty",
+    "hash": "39a5a6b",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "bad02ca",
+    "suffix": "39a5a6b-dirty",
     "semverString": null,
     "version": "2.0.21"
 };
