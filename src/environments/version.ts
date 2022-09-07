2b96319b4a45a7cde9844d8ebf8f7ec7a1aa9e3d
revathy260 <75604760+revathy260@users.noreply.github.com>
2022-09-07 17:28:18 +0530

    version udpate

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": true,
-    "raw": "9ab96d5-dirty",
-    "hash": "9ab96d5",
+    "dirty": false,
+    "raw": "be70f78",
+    "hash": "be70f78",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "9ab96d5-dirty",
+    "suffix": "be70f78",
     "semverString": null,
     "version": "2.0.11"
 };
