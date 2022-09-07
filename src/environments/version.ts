57633cdcd55a3bd8a5eab501b99a2b5b360ab57b
revathy260 <75604760+revathy260@users.noreply.github.com>
2022-09-07 17:30:29 +0530

    ver 12

--- src/environments/version.ts
+++ src/environments/version.ts
@@ -1,13 +1,13 @@
 // IMPORTANT: THIS FILE IS AUTO GENERATED! DO NOT MANUALLY EDIT OR CHECKIN!
 /* tslint:disable */
 export const VERSION = {
-    "dirty": false,
-    "raw": "be70f78",
-    "hash": "be70f78",
+    "dirty": true,
+    "raw": "2b96319-dirty",
+    "hash": "2b96319",
     "distance": null,
     "tag": null,
     "semver": null,
-    "suffix": "be70f78",
+    "suffix": "2b96319-dirty",
     "semverString": null,
     "version": "2.0.11"
 };
