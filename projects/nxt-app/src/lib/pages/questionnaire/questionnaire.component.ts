478360ceac4029da683a280f25acbb2b20ce23f0
vijay <vijay@rangerinc.co>
2023-07-28 10:46:19 +0530

    datepicker update

--- projects/nxt-app/src/lib/pages/questionnaire/questionnaire.component.ts
+++ projects/nxt-app/src/lib/pages/questionnaire/questionnaire.component.ts
@@ -2293,7 +2293,7 @@ Add(question: LocalQuestion){
 
   displayDate(dateSelected: any,ques:any){
     // Parse the date string using moment and assign it to this.selectedDate
-    ques.input = dateSelected.value;
+    ques.input = moment(dateSelected.value , 'YYYY-MM-DD HH:mm:ss').format('YYYY-MM-DD HH:mm:ss').toString();
   }
 
   getKey(item: any): any {
